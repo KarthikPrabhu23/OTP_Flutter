6fa31ba0007a6c7cb2cfdd99550376d9d9d2440b
karthik <karthikprabhu23223@gmail.com>
2023-06-11 01:57:41 +0530

    commit2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:otp1/provider/auth_provider.dart';
 import 'package:otp1/screens/welcome_screen.dart';
